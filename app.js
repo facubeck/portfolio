886869a18a8c58776bbd9ddfcf6ab56b0b2ade62
Facundo <facundobeck@gmail.com>
2023-11-06 14:03:17 -0300

    Contact Links and Copy Bubble when click

--- app.js
+++ app.js
@@ -8,3 +8,33 @@ projectTitles.forEach((title) => {
     event.target.style.color = "black";
   });
 });
+
+let link = document.getElementById('mail');
+let copyBubble = document.querySelector('.copy-bubble')
+
+link.addEventListener('click', () => {
+    navigator.clipboard.writeText(link.textContent);
+    copyBubble.textContent = 'Copied!'
+})
+
+link.addEventListener('mouseover', (event) => {
+    copyBubble.style.opacity = '1';
+    event.target.style.color = 'black';
+})
+link.addEventListener('mouseout', (event) => {
+    copyBubble.style.opacity = '0';
+    event.target.style.color = 'rgba(0, 0, 0, 0.7)';
+    setTimeout(() => {
+        copyBubble.textContent = 'Copy to clipboard!';
+    }, 500);
+})
+
+const links = document.querySelectorAll('.link');
+links.forEach((link) => {
+    link.addEventListener('mouseover', e => {
+        e.target.style.color = 'black'
+    });
+    link.addEventListener('mouseout', e => {
+        e.target.style.color = 'rgba(0, 0, 0, 0.7)'
+    });
+})
