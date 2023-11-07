2f0568d7d248e34ddfd65df43e75dad6b5ba6a07
Facundo <facundobeck@gmail.com>
2023-11-06 21:41:08 -0300

    Finished animation of triangles

--- app.js
+++ app.js
@@ -9,32 +9,51 @@ projectTitles.forEach((title) => {
   });
 });
 
-let link = document.getElementById('mail');
-let copyBubble = document.querySelector('.copy-bubble')
+let link = document.getElementById("mail");
+let copyBubble = document.querySelector(".copy-bubble");
 
-link.addEventListener('click', () => {
-    navigator.clipboard.writeText(link.textContent);
-    copyBubble.textContent = 'Copied!'
-})
+link.addEventListener("click", () => {
+  navigator.clipboard.writeText(link.textContent);
+  copyBubble.textContent = "Copied!";
+});
 
-link.addEventListener('mouseover', (event) => {
-    copyBubble.style.opacity = '1';
-    event.target.style.color = 'black';
-})
-link.addEventListener('mouseout', (event) => {
-    copyBubble.style.opacity = '0';
-    event.target.style.color = 'rgba(0, 0, 0, 0.7)';
-    setTimeout(() => {
-        copyBubble.textContent = 'Copy to clipboard!';
-    }, 500);
-})
+link.addEventListener("mouseover", (event) => {
+  copyBubble.style.opacity = "1";
+  event.target.style.color = "black";
+});
+link.addEventListener("mouseout", (event) => {
+  copyBubble.style.opacity = "0";
+  event.target.style.color = "rgba(0, 0, 0, 0.7)";
+  setTimeout(() => {
+    copyBubble.textContent = "Copy to clipboard!";
+  }, 500);
+});
 
-const links = document.querySelectorAll('.link');
+// Change link color when hover
+const links = document.querySelectorAll(".link");
 links.forEach((link) => {
-    link.addEventListener('mouseover', e => {
-        e.target.style.color = 'black'
-    });
-    link.addEventListener('mouseout', e => {
-        e.target.style.color = 'rgba(0, 0, 0, 0.7)'
-    });
+  link.addEventListener("mouseover", (e) => {
+    e.target.style.color = "black";
+  });
+  link.addEventListener("mouseout", (e) => {
+    e.target.style.color = "rgba(0, 0, 0, 0.7)";
+  });
+});
+
+// Make triangles rotate
+const triangles = document.querySelectorAll("#triangle");
+triangles.forEach((div) => {
+    let element = div;
+    element.addEventListener('mouseenter', e => {
+        console.log(e);
+        for(const child of e.target.children) {{
+            child.style.transform = 'rotate(180deg)'
+        }}
+    })
+    element.addEventListener('mouseleave', e => {
+        console.log(e);
+        for(const child of e.target.children) {{
+            child.style.transform = 'rotate(360deg)'
+        }}
+    })
 })
